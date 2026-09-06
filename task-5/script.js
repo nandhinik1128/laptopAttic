08f220c848a2ed718f5ba19366cfc0a47e733c6c
nandhinik1128 <nandhini.k1128@gmail.com>
2026-09-06 11:49:07 +0530

    script

---
+++ task-5/script.js
@@ -0,0 +1,20 @@
+let mark="95";
+mark=Number(mark);
+switch(true){
+    case (mark>=90 && mark<=100):
+        console.log("A");
+        break;
+        case (mark>=75 && mark<=89):
+            console.log("B");
+            break;
+            case (mark>=50 && mark<=74):
+                console.log("C");
+                break;
+               default:
+                    console.log("Fail");
+                    
+
+                   
+                    
+
+}
